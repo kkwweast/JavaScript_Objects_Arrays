5ca7dc5f6208053b43ba77a014c626edbe992e65
Kim Weast <weastkim3@gmail.com>
2023-10-15 16:30:16 -0500

    update

---
+++ app.js
@@ -0,0 +1,66 @@
+ //Sum of Array
+ console.log("Exercise 1:\n")
+ const testArr = [2, 22, 12, 17, 18, 39, 129] ;
+
+
+ function arraySum(numbers){ 
+    let sum = 0;
+
+    //Normal for loop
+    for (let i = 0; i < number.length; i++)
+ sum += number
+//return sum;
+//for each Method
+
+//Using the array reduce method
+
+ let sum = arr.reduce((sum, number) => { 
+   return sum + number;
+
+}
+ 
+
+return sum;
+;
+
+ // Exercise 2 Section
+console.log("EXERCISE 2:\n==========\n");
+var Book =  {};
+     book.title = "Things We Left Behind";
+     book.pages = 592;
+     book.readCount = 3;
+     Book.info = function () { 
+    return ' ${this. title}, ${this.pages} pages,   
+        const read   ${this. readCount}, times
+    }
+
+
+
+ 
+
+
+
+
+
+
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+//reverse every word in a sentence
+let sentence = "The quick brown fox jumps over the lazy dog"; 
+
+
+function reverseWords(sentence ){
+    let results = [];
+    //Identify the words based on spaces
+    let words = sentence.split("");
+    for (let i = 0; i < words.length; i++) {
+        let letters = words[i].split(""); // ex "The"  => ["!", "h", "e"]
+        letters.reverse(); //ex ["e", "h, "!"] 
+        result = result.concat(letters);
+    }
+    return result.join("");
+
+}
+console.log(reverseWords(sentence));
+console.log(reverseWords("My name is Ben"));
+console.log(reverseWords("I am Ironman"));
