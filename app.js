109dd233236b27f5e572ff7a85d91213367354b6
Kim Weast <weastkim3@gmail.com>
2023-11-01 16:56:44 -0500

    Committing local changes to app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
  //Sum of Array
  console.log("Exercise 1:\n")
  const testArr = [2, 22, 12, 17, 18, 39, 129] ;
@@ -6,73 +6,94 @@
 
  function arraySum(numbers){ 
     let sum = 0;
-
+ }
     //Normal for loop
-    for (let i = 0; i < number.length; i++)
- sum += number
+   // for (let i = 0; i < number.length; i++)
+// sum += number
 //return sum;
 //for each Method
-
-//Using the array reduce method
-
- let sum = arr.reduce((sum, number) => { 
-   return sum + number;
-
-}
- 
+numbers.ForEach(function(number) {
+  sum += number;
+});
 
 return sum;
-;
-
- // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
-var Book =  {};
-     book.title = "Things We Left Behind";
-     book.pages = 592;
-     book.readCount = 3;
-     Book.info = function () { 
-    return ' ${this. title}, ${this.pages} pages,   
-        const read   ${this. readCount}, times
-    }
 
 
-
- 
-
-
-
-
-
-
-// Exercise 3 Section
-console.log("EXERCISE 3:\n==========\n");
-//reverse every word in a sentence
-let sentence = "The quick brown fox jumps over the lazy dog"; 
-
-
-function reverseWords(sentence ){
-    let results = [];
-    //Identify the words based on spaces
-    let words = sentence.split("");
-    for (let i = 0; i < words.length; i++) {
-        let letters = words[i].split(""); // ex "The"  => ["!", "h", "e"]
-        letters.reverse(); //ex ["e", "h, "!"] 
-        result = result.concat(letters);
-    }
-    return result.join("");
-
-}
-console.log(reverseWords(sentence));
-console.log(reverseWords("My name is Ben"));
-console.log(reverseWords("I am Ironman"));
-=======
-console.log("Hello World!\n==========\n");
-
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+var Book =  {};
+     book.title = "Dracula";
+     book.author = "Bram Stoker"
+     book.pages = 230;
+     book.readCount = 15;
+     Book.info = function () { 
+    return `  ${this. title} ,${this.pages} pages, read ${this.readCount} times`; 
+         
+    }
+    console.log(book.info);
 
->>>>>>> 14ca83267fb3910f4d5027cac1bf44663b340e8a
+    //Exercise 3:
+    let sentence = "The quick brown fox jumps over the lazy dog"; //string
+    //method Chaining Example
+    sentence.split("").map((element, index, arr) => {
+      return element.split("").reverse().join("");
+    }).join("");
+    // string => array (split method)
+    const words = sentence.split("");
+    console.log(words);
+
+    //Loop (array)
+    for(let i = 0; i < words.length; i++) {
+//Each element in the array
+const letters = words[i].split("");
+console.log(letters);
+    //Array => array
+    //updates the array dirctly.No need to store into a new variable
+    letters.reverse();
+    const reversedLetters = letters.joim("");
+    words[i] = reversedLetters;
+}
+console.log(words);
+    //array => string
+    const result = words.join("");
+    console.log(result);
+
+    //Exercise 4:
+    let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
+
+    //2.
+    const rows = csvData.split("\n");
+    const headers = rows[0].split(",");
+    for(let i = 1; i < rows.length; i++){
+      const pairs = rows[i].split(",");
+       let hobbit = {};
+       hobbit.name = pairs[0];
+       hobbit.age = pairs[1];
+      //storage step
+      listofHobbits.push(hobbit);
+       finalResult.push(hobbit);
+    }
+
+    
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
